13727630860fe64ef60bbefbffd9851ba7da44c6
Ali Hejazizo <hejazizo@ualberta.ca>
2022-11-11 12:18:03 -0500

    Update material

---
+++ Python/02. Object Oriented Programming/images/Initial_project/railway.py
@@ -0,0 +1,40 @@
+class Train:
+    
+    def __init__(self, last_visited_city, weight_capacity, is_on_trip):
+        self.last_visited_city = last_visited_city
+        self.weight_capacity = weight_capacity
+        self.is_on_trip = is_on_trip
+
+
+class Trip:
+
+    all_cities = ('Arak', 'Ardabil', 'Urmia', 'Isfahan', 'Ahvaz', 'Ilam', 'Bojnord', 'Bandar Abbas', 'Bushehr', 'Birjand', 'Tabriz', 'Tehran', 'Khorramabad', 'Rasht', 'Zahedan', 'Zanjan', 'Sari', 'Semnan', 'Sanandaj', 'Shahr-e Kord', 'Shiraz', 'Qazvin', 'Qom', 'Karaj', 'Kermanshah', 'Gorgan', 'Mashhad', 'Hamadan', 'Yasuj', 'Yazd')
+
+    def __init__(self, origin_city, destination_city, train):
+        self.train = self.train_validation(train)
+        self.destination_city = destination_city
+        self.origin_city = self.origin_city_validation(origin_city)
+        self.passengers = []
+    
+    def origin_city_validation(self, origin_city):
+        pass
+        
+    def train_validation(self, train):
+        pass
+
+    # here implement the magic method
+
+
+class Passenger:
+
+    def __init__(self, fullname, load_weight):
+        self.fullname = fullname
+        self.load_weight = load_weight
+
+    def attend_trip(self, trip):
+        pass
+
+    def cancel_trip(self, trip):
+        pass
+
+    # here implement the magic method
